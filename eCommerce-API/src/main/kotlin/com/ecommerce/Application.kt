ff005b05d19744497c2f8c12c0aa74482122cf86
Matteo Cagno <matteo.cagno@thoughtworks.com>
2023-11-22 14:53:02 +0100

    Added CORS support

--- eCommerce-API/src/main/kotlin/com/ecommerce/Application.kt
+++ eCommerce-API/src/main/kotlin/com/ecommerce/Application.kt
@@ -3,6 +3,7 @@ package com.ecommerce
 import com.ecommerce.DI.DIModule
 import com.ecommerce.plugins.configureRouting
 import com.ecommerce.plugins.configureSerialization
+import com.example.plugins.configureHTTP
 import io.ktor.server.application.*
 
 import org.koin.ktor.plugin.Koin
@@ -15,7 +16,7 @@ fun Application.module() {
     install(Koin) {
         modules(DIModule)
     }
-
+    configureHTTP()
     configureSerialization()
     configureRouting()
 }
